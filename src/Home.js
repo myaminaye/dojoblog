4b228b30c7b1b64ffecdbbb94b724068a1471faf
myaminaye <myaminaye22@gmail.com>
2022-09-01 09:40:34 +0630

    first

---
+++ src/Home.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+    // const handleClick = () => {
+    //     console.log('Hello');
+    // }
+
+    // const handleClickAgain = (name) => {
+    //     console.log('Hello ' + name);
+    // }
+
+    // const [name,setName] = useState('Emerald');
+    // const [age, setAge] = useState(20);
+
+    // const handleClick = () =>{
+    //     setName('Beryl');
+    //     setAge(22);
+    // }
+
+    const [blogs, setBlogs] = useState(null);
+
+    const [name,setName] = useState('emerald');
+
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter((blog)=> blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(()=> {
+        fetch('http://localhost:3000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            console.log(data);
+        })
+    },[])
+
+    return ( 
+        <div className="home">
+            
+            {/* <button onClick={handleClick}>Click Me</button> */}
+            {/* <button onClick={()=>{handleClickAgain('Emerald')}}>Click Again</button> */}
+
+            {/* <p>{ name } is {age}</p> */}
+            {/* <button onClick={handleClick}>Click</button> */}
+
+            {blogs && <BlogList blogs={blogs} title= "All Blogs" handleDelete={handleDelete} />}
+            {/* <BlogList blogs={blogs.filter((blog)=>blog.author==='mario')} title= "Mario's Blogs" /> */}
+            {/* <button onClick={()=> setName('beryl')}>Change Name</button> */}
+            {/* {name} */}
+        </div>
+     );
+}
+ 
+export default Home;
